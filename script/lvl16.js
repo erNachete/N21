050e5856295e7e78565dbdf9aeda6d09608c4440
GaaraDeadPuppet <ikani_93@hotmail.com>
2015-11-27 17:58:21 +0100

    Create lvl16.js

---
+++ script/lvl16.js
@@ -0,0 +1,16 @@
+function clicar(){
+	$("#aceptar").click(confirmar);
+}
+
+function confirmar(){
+	var confirmar=confirm("perfecto! Asi me gusta! Espera a la contraseña.\n Dale a aceptar para continuar.");
+	if(confirmar){
+		window.open("index.html",'_system');
+	}else{
+		alert("uoooooo, vuelve a intentarlo");
+	}
+}
+
+window.onload = function(){
+	clicar();
+}
