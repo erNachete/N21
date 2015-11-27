d7c7f6545211a55a997067c8879357d327da36a8
GaaraDeadPuppet <ikani_93@hotmail.com>
2015-11-27 11:37:58 +0100

    abridor

--- script/script.js
+++ script/script.js
@@ -1,11 +1,26 @@
+
 function darclick(){
 	for(var i=0;i<$("div").length;i++){
-		$("div").click(abrir);
+		$("#lvl"+i).click(abrir);
 	}
 }
 
 function abrir(){
-	window.open(this.id+".html",'_system');
+	var contrasena = prompt("Introduzca la contraseña señorita nerea", "");
+	var codigo = parseInt((this.id).replace( /^\D+/g, ''));
+	var respuesta;
+	
+	switch(codigo){
+		case 0: resepuesta="21";break;
+	}
+	
+	for(var i=0;i<$("#lvl"+codigo).length;i++){
+		if(contrasena==resepuesta){
+			window.open(this.id+".html",'_system');
+		}else{
+			alert("Listillaaaaa, sigue las instruccioens :P");
+		}
+	}
 }
 
 window.onload = function(){
