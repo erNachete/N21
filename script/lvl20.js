7fcace5f006b95945a72ce0a4517da648cd78f61
GaaraDeadPuppet <ikani_93@hotmail.com>
2015-11-27 18:58:01 +0100

    Create lvl20.js

---
+++ script/lvl20.js
@@ -0,0 +1,20 @@
+function clicar(){
+	$("#aceptar").click(confirmar);
+}
+
+function confirmar(){
+	var comida=$("#respuesta").val();
+	comida=comida.toLowerCase();
+	if(comida=="mad mansion"){
+		var confirmar=confirm("yujuuu, vamos a pasarlo genial! la proxima contraseña es el numero del episodio que vamos a jugar.\n Dale a aceptar para continuar.");
+		if(confirmar){
+			window.open("index.html",'_system');
+		}
+	}else{
+		alert("en serio?");
+	}
+}
+
+window.onload = function(){
+	clicar();
+}
