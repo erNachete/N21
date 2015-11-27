54a61f27e9aab4d1f9265e67a950d4d9241740e5
GaaraDeadPuppet <ikani_93@hotmail.com>
2015-11-27 17:30:59 +0100

    Create lvl11.js

---
+++ script/lvl11.js
@@ -0,0 +1,23 @@
+var contador=0;
+
+function clicar(){
+	$("#aceptar").click(confirmar);
+	$("#cancelar").click(cancelar);
+}
+
+function cancelar(){
+	alert("En serio? No mientas anda...");
+}
+
+function confirmar(){
+	var confirmar=confirm("perfecto! Asi me gusta! La contraseña para el proximo nivel es el curso en el que estas en la uni.\n Dale a aceptar para continuar.");
+	if(confirmar){
+		window.open("index.html",'_system');
+	}else{
+		alert("uoooooo, vuelve a intentarlo");
+	}
+}
+
+window.onload = function(){
+	clicar();
+}
