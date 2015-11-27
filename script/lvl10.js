15d5964ff704374b04090e158db3585a8176ccde
GaaraDeadPuppet <ikani_93@hotmail.com>
2015-11-27 17:24:40 +0100

    Create lvl10.js

---
+++ script/lvl10.js
@@ -0,0 +1,20 @@
+function clicar(){
+	$("#aceptar").click(confirmar);
+}
+
+function confirmar(){
+	var comida=$("#respuesta").val();
+	comida=comida.toLowerCase();
+	if(comida=="daw"){
+		var confirmar=confirm("perfecto! Asi me gusta! La contraseña para el proximo nivel es el nombre de uno de los personajes de el internado (principales).\n Dale a aceptar para continuar.");
+		if(confirmar){
+			window.open("index.html",'_system');
+		}
+	}else{
+		alert("uoooooo, vuelve a intentarlo");
+	}
+}
+
+window.onload = function(){
+	clicar();
+}
